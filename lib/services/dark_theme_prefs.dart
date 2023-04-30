46430e6a937bf21674854c760e0e108c0da1629c
saci zakaria <sacidevelppeur@gmail.com>
2023-04-30 22:39:03 +0200

    SharedPreferences.getInstance();

--- lib/services/dark_theme_prefs.dart
+++ lib/services/dark_theme_prefs.dart
@@ -1,10 +1,9 @@
-
 import 'package:shared_preferences/shared_preferences.dart';
 
-class DarkThemePrefs{
+class DarkThemePrefs {
   static const THEME_STATUS = "THEMESTATUS";
+
   setDarkTheme(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(THEME_STATUS, value);
   }
 }
